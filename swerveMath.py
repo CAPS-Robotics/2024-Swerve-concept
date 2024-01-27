9f0acdf0c62da273810ca5018a42ecb27914b36a
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-01-27 10:48:04 -0600

    Update swerveMath.py

--- swerveMath.py
+++ swerveMath.py
@@ -4,26 +4,26 @@ import math
 
 # Values you get
 r = 15*1.41 # numerical distance from corner to center on 30x30, r variable is dist. from robot center to pivot
-wb = 30 # wheelbase
-wt = 30 # wheeltrack
+wb = 30 # wheelbase, for 30x30 bot
+wt = 30 # wheeltrack, for 30x30 bot
 
-da = 10 # change in angle, sensor
-dt = 10 # change in time, sensor
+da = 10 # change in angle, arb. value, sensor measured
+dt = 10 # change in time, arb. value, sensor measured
 
 # angular velocity of the wheel
 wz = da/dt 
 
 # linear velocity
-speed = 10 # speed of the bot, In RPM, sensor
-angle = 10 # angle of the bot, gyro sensor
-v = 0 # linear velocity vector
+speed = 10 # speed of the bot, In RPM, arb. value, sensor measured
+angle = 10 # angle of the bot, gyro, arb. value, sensor measured
+v = 0 # linear velocity vector, arb. value, sensor measured
 v = (v * math.cos(angle), v * math.sin(angle))
 
 # resultant velocity
 vw = v + wz
 
-vx = 0 # x-component
-vy = 1 # y-component
+vx = 0 # x-component, arb. value, sensor measured
+vy = 1 # y-component, arb. value, sensor measured
 
 rx = wb/2
 ry = wt/2
@@ -31,7 +31,7 @@ ry = wt/2
 # components
 vfrx = vx + rx*wz
 vfry = vy + ry*wz
-wheel_r = 10 # wheel radius
+wheel_r = 10 # wheel radius, arb. value, get real measurements of the wheel you use
 
 steer_angle = math.atan(vfry/vfrx)
 wheel_angular = (vfrx**2+vfry**2)**0.5/wheel_r
@@ -45,7 +45,7 @@ wheel_angular = (vfrx**2+vfry**2)**0.5/wheel_r
 # two steps, compute drift from joint states, update pose over time
 
 # computing the xy changes for wheels
-wfr = 0 # wheel angular velocity
+wfr = 0 # wheel angular velocity, arb. value, sensor measured
 vfrx = wfr * wheel_r * math.cos(steer_angle)
 vfry = wfr * wheel_r * math.sin(steer_angle)
 
@@ -64,7 +64,7 @@ a = a + wz*Dt
 
 # multiply all velocities for scaling factors for safety
 
-max_m = 50 # max speed a motor can be, arb. value
-max_a = 10 # max ang. vel. a motor can be, arb. value
+max_m = 50 # max speed a motor can be, arb. value, get motor measurements
+max_a = 10 # max ang. vel. a motor can be, arb. value, get motor measurements
 
-scale_f = max_m/max_a
+scale_f = max_m/max_a # scale factor to multiply values by
